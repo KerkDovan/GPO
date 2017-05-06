429650a5712c72eae55a6ac4d34e0d893899df10
Бодрухин Александр <bodro96@gmail.com>
2017-05-06 14:25:26 +0700

    Yay!

--- quiz/js/controller.js
+++ quiz/js/controller.js
@@ -1,28 +1,45 @@
-function main() {
-	console.log('JS Loaded!');
+var quiz_title;
+var quiz_desc;
+var quiz_ref;
+var quiz_testtype;
 
-	var quiz_title;
-	var quiz_desc;
-	var quiz_ref;
-	var quiz_testtype;
+var quiz_questions = new Array();
+var number_of_questions;
 
-	var quiz_questions = new Array();
-	var number_of_questions;
+$(document).ready($.getJSON('json/quiz.json', function (data) {
+	quiz_title = data.title;
+	quiz_desc = data.description;
+	quiz_ref = data.reference;
+	quiz_testtype = data.testtype;
 
-	$.getJSON('json/quiz.json', function(data) {
-		quiz_title = data.meta.title;
-		quiz_desc = data.meta.description;
-		quiz_ref = data.meta.reference;
-		quiz_testtype = data.meta.testtype;
-
-		for (i = 0; i < data.questions.length; i++) {
-			quiz_questions[i] = new Array;
-			quiz_questions[i][0] = data.questions[i].question;
+	for (i = 0; i < data.questions.length; i++) {
+		quiz_questions[i] = new Array;
+		quiz_questions[i][0] = data.questions[i].question;
+		for (j = 0; j < data.questions[i].options.length; j++) {
+			quiz_questions[i][j + 1] = data.questions[i].options[j];
 		}
-		number_of_questions = quiz_questions.length;
-	})
+	}
+	number_of_questions = quiz_questions.length;
 
-	console.log(quiz_title);
+	$('title').text(quiz_title);
+	print_quiz();
+}))
+
+function print_quiz() {
+	$('#quiz_title').text(quiz_title);
+	$('#quiz_desc').text(quiz_desc);
+	$('#quiz_ref').text(quiz_ref);
+	$('#quiz_ref').append('<br/><br/>');
+	for (i = 0; i < number_of_questions; i++) {
+		$('#quiz_questions').append('<div>');
+		$('#quiz_questions').append(quiz_questions[i][0]);
+		$('#quiz_questions').append('<p>');
+		for (j = 1; j < quiz_questions[i].length; j++) {
+			$('#quiz_questions').append(
+				'<input type="checkbox" name="option"' + j + ' value="a' + j + '">' + quiz_questions[i][j] + '<br/>');
+		}
+		$('#quiz_questions').append('</p>');
+		$('#quiz_questions').append('</div><br/>');
+	}
 
 }
-$(document).ready(main)
